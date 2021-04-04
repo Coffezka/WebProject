e0230759f31dbffeeda4db9f47971a5b01dbcfae
Coffezka <42944711+Coffezka@users.noreply.github.com>
2021-04-04 14:13:08 +0300

    Initial commit

---
+++ manageMoneyApp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import currencie
+from .models import usersBill
+from .models import billCurrencie
+from .models import userGoal
+from .models import userHistory
+from .models import userSetting
+
+# Register your models here.
+admin.site.register(currencie)
+admin.site.register(usersBill)
+admin.site.register(billCurrencie)
+admin.site.register(userGoal)
+admin.site.register(userHistory)
+admin.site.register(userSetting)
---
+++ manageMoneyApp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class currencie(models.Model):
+    name = models.CharField(max_length=3) #Стандартом ISO 4217
+    fullName = models.CharField(max_length=30)
+    value = models.IntegerField()
+
+class usersBill(models.Model):
+    userID = models.CharField(max_length=400)#FK
+    billName = models.CharField(max_length=30)
+    img = models.CharField(max_length=400)
+
+class billCurrencie(models.Model):
+    billID = models.ForeignKey(usersBill, on_delete=models.CASCADE)
+    currencieID = models.ForeignKey(currencie, on_delete=models.CASCADE)
+    balance = models.IntegerField()
+
+class userGoal(models.Model):
+    userID = models.CharField(max_length=400)#FK
+    dateStart = models.DateTimeField()
+    dateEnd = models.DateTimeField()
+    description = models.CharField(max_length=500)
+    goalSum = models.IntegerField()
+
+class userHistory(models.Model):
+    userID = models.CharField(max_length=400)#FK
+    date = models.DateTimeField()
+    billID = models.ForeignKey(usersBill, on_delete=models.CASCADE)
+    function = models.IntegerField()
+    category = models.IntegerField()
+    sum = models.IntegerField()
+    currencieID = models.ForeignKey(currencie, on_delete=models.CASCADE)#FK
+
+class userSetting(models.Model):
+    userID = models.CharField(max_length=400)#FK
+    darkTheme =  models.BooleanField()
+    defaultCurrencie = models.ForeignKey(currencie, on_delete=models.CASCADE)
