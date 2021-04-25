8328ef3e19cef509457a4a918966e3ef5dae428b
Coffezka <42944711+Coffezka@users.noreply.github.com>
2021-04-25 21:26:08 +0300

    Access

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',  # <-- And here
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # <-- And here
     ],
 }
 
--- manageMoneyApp/views.py
+++ manageMoneyApp/views.py
@@ -1,34 +1,41 @@
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import authentication,permissions
 from .serializers import currencieSerializer, usersBillSerializer, billCurrencieSerializer, userGoalSerializer,userHistorySerializer, userSettingSerializer
 from .models import currencie, usersBill, billCurrencie, userGoal, userHistory, userSetting
+from rest_framework.response import Response
 
 class currencieViewSet(viewsets.ModelViewSet):
     queryset = currencie.objects.all()
     serializer_class = currencieSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
 
 class usersBillViewSet(viewsets.ModelViewSet):
     queryset = usersBill.objects.all()
     serializer_class = usersBillSerializer
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
 class billCurrencieViewSet(viewsets.ModelViewSet):
     queryset = billCurrencie.objects.all()
     serializer_class = billCurrencieSerializer
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
 class userGoalViewSet(viewsets.ModelViewSet):
     queryset = userGoal.objects.all()
     serializer_class = userGoalSerializer
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
+
 
 class userHistoryViewSet(viewsets.ModelViewSet):
     queryset = userHistory.objects.all()
     serializer_class = userHistorySerializer
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
 class userSettingViewSet(viewsets.ModelViewSet):
     queryset = userSetting.objects.all()
     serializer_class = userSettingSerializer
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
