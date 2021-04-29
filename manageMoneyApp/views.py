1c742f0d16aed331559e6067776f9f9d8d11b456
Coffezka <42944711+Coffezka@users.noreply.github.com>
2021-04-29 14:23:08 +0300

    Update

--- manageMoneyApp/views.py
+++ manageMoneyApp/views.py
@@ -2,8 +2,12 @@ from rest_framework import viewsets
 from rest_framework import authentication,permissions
 from .serializers import currencieSerializer, usersBillSerializer, billCurrencieSerializer, userGoalSerializer,userHistorySerializer, userSettingSerializer
 from .models import currencie, usersBill, billCurrencie, userGoal, userHistory, userSetting
-from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import filters
+
+class OwnerFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(userID=request.user)
 
 class currencieViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = currencie.objects.all()
@@ -11,6 +15,7 @@ class currencieViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class usersBillViewSet(viewsets.ModelViewSet):
+    filter_backends = (OwnerFilterBackend,)
     queryset = usersBill.objects.all()
     serializer_class = usersBillSerializer
     authentication_classes = [authentication.TokenAuthentication]
@@ -23,19 +28,23 @@ class billCurrencieViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class userGoalViewSet(viewsets.ModelViewSet):
+    filter_backends = (OwnerFilterBackend,)
     queryset = userGoal.objects.all()
     serializer_class = userGoalSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
+    
 
 
 class userHistoryViewSet(viewsets.ModelViewSet):
+    filter_backends = (OwnerFilterBackend,)
     queryset = userHistory.objects.all()
     serializer_class = userHistorySerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
 class userSettingViewSet(viewsets.ModelViewSet):
+    filter_backends = (OwnerFilterBackend,)
     queryset = userSetting.objects.all()
     serializer_class = userSettingSerializer
     authentication_classes = [authentication.TokenAuthentication]
